204191237443f6bcc3cd23673545d8c48e16d5cf
theorivero <thrivero1@gmail.com>
2020-09-22 08:29:12 -0300

    add client class with init, return, update funcs

---
+++ client.py
@@ -0,0 +1,28 @@
+from uuid import uuid4
+
+class client:
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+        self.uid = uuid4().int
+        self.rented_equipment = []
+
+    def return_name(self):
+        return self.name
+
+    def update_name(self,new_name):
+        self.name = new_name
+        return self.name
+
+    def return_phone_number(self):
+        return self.phone_number
+
+    def update_phone_number(self, new_phone_number):
+        self.phone_number = new_phone_number
+        return self.phone_number
+
+    def return_rented_equipment(self):
+        return self.rented_equipment
+
+pedro = client('pedro', '48999999') 
+print(pedro.return_rented_equipment())
