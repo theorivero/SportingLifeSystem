7493d94faff1a772fbb3e77bd7517b4236d63cd2
theorivero <thrivero1@gmail.com>
2020-09-24 09:55:50 -0300

    add register_equipament func

--- menu.py
+++ menu.py
@@ -31,7 +31,19 @@ class Menu:
         new_customer = Customer(name, phone_number)
         self.__customer_base.append(new_customer)
         time.sleep(1)
-        print('Cliente cadastrado com sucesso')
+        print(f'Cliente {name} cadastrado com sucesso')
+
+    def register_equipament(self):
+        #name, total_quantity, available_quantity, rental_price
+        name = input('Qual equipamento você quer cadastrar: ')
+        total_quantity = input('Você tem quantos equipamentos desse tipo: ')
+        available_quantity = input('Quantos estão disponiveis hoje: ')
+        rental_price = input('Quanto custa o aluguel mensal desse equipamento: ')
+        new_equipament = Equipment(name, total_quantity, available_quantity, rental_price)
+        self.__stock.append(new_equipament)
+        time.sleep(1)
+        print(f'Equipamento {name} cadastrado no estoque')
+
     
     def start(self):
         choice = 0
@@ -52,11 +64,14 @@ Por favor escolha a opção: """))
                 self.register_customer()
                 time.sleep(1)
             if choice == 2: 
-                print('Escolha 2')
+                self.register_equipament()
+                time.sleep(1)
             if choice == 3: 
                 for customer in self.customer_base: 
                     print( customer.name, customer.phone_number )
                 time.sleep(4)
             if choice == 4: 
-                print('Escolha 4')
+                for equipment in self.stock: 
+                    print( equipment.name, equipment.total_quantity, equipment.available_quantity, equipment.rental_price)
+                time.sleep(4)
         print('Sistema fechado com sucesso')
